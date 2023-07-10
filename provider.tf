8e6669f24e734c75f9709c74bdc43a41abe58d06
mukulgavri <127583898+mukulgavri@users.noreply.github.com>
2023-07-10 22:32:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,30 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "new-aws-bucket-mg"
-    region = "us-east-1"
-    key    = "terraform.tfstate"
-  }
-}
-
-data "aws_ami" "newami" {
-  most_recent = true
-  owners      = ["679593333241"]
-
-  filter {
-    name   = "name"
-    values = ["Appgate-SDP*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
 
 provider "aws" {
   region     = var.region
